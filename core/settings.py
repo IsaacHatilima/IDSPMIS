acf708c7104099ba58d1e9862bf1496265897109
Isaac Hatilima <isaachatilima@gmail.com>
2021-11-29 11:38:27 +0200

    Static Files

--- core/settings.py
+++ core/settings.py
@@ -167,7 +167,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join('/home/idsprrud/static')
+STATIC_ROOT = os.path.join('/home/idsprrud/public_html/static')
 # STATIC_ROOT = 'staticfiles'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
