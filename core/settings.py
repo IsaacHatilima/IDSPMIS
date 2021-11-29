71de585bf73abba34d334522795e6dfc50ee1a95
Isaac Hatilima <isaachatilima@gmail.com>
2021-11-29 18:04:46 +0200

    Document issue

--- core/settings.py
+++ core/settings.py
@@ -168,14 +168,15 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join('/home/idsprrud/static')
+# STATIC_ROOT = os.path.join('/home/idsprrud/static')
+STATIC_ROOT = os.path.join('/home/idsprrud/ims.idspzambia.org/static')
 # STATIC_ROOT = 'staticfiles'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = os.path.join(BASE_DIR, '/home/idsprrud/public_html/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/home/idsprrud/ims.idspzambia.org/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
